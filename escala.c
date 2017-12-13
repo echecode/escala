1f1f6d23d519d9da53100c062a5d90db20542712
Hernán Eche <hernan_eche@yahoo.com.ar>
2017-12-13 15:32:45 -0300

    Update escala.c

--- escala.c
+++ escala.c
@@ -13,7 +13,7 @@ float fit(float in0,float from0,float to0,float fromFix0,float toFix0){
 	if(den!=0){
 		//a1x+b1y=c1
 		//a2x+b2y=c2
-		//cramer
+		//regla de Cramer
 		float x=(b1*c2-b2*c1)/den;
 		float y=(a2*c1-a1*c2)/den;
 		res=in0*x+y;
